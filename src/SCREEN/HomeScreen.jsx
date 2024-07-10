6b33e754e61a80d420b1ff15b5cb82eddf84c844
elsacodesid <elsacodesid@gmail.com>
2024-07-10 13:30:07 +0700

    update

--- src/SCREEN/HomeScreen.jsx
+++ src/SCREEN/HomeScreen.jsx
@@ -1,14 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {colors} from '../utils/colors';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/logo.png')} style={styles.logo} />
+      <Image
+        source={require('../assets/login-img.png')}
+        style={styles.loginImg}
+      />
+      <Text style={styles.text}>Lorem, ipsum dolor.</Text>
     </View>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+    alignItems: 'center',
+  },
+  logo: {
+    height: 40,
+    width: 140,
+    marginVertical: 20,
+  },
+  loginImg: {
+    height: 250,
+    width: 231,
+    marginVertical: 30,
+  },
+  text: {
+    fontSize: 36,
+  },
+});
