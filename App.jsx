f17b78bc8bf00bd23b6eae16dc1764562e1de9d7
elsacodesid <elsacodesid@gmail.com>
2024-07-10 12:55:09 +0700

    update

--- App.jsx
+++ App.jsx
@@ -1,18 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
+import HomeScreen from './src/SCREEN/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>App</Text>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name={"HOME"} component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
