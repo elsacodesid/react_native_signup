2259670abdbcd9c945a61a90d84535c7fa6e3bd4
elsacodesid <elsacodesid@gmail.com>
2024-07-11 15:17:27 +0700

    update

--- App.jsx
+++ App.jsx
@@ -3,14 +3,17 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './src/SCREEN/HomeScreen';
+import LoginScreen from './src/SCREEN/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name={"HOME"} component={HomeScreen} />
+        <Stack.Screen name={"LOGIN"} component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
